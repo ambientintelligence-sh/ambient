6fa19d71647cfc1d8cbec45df83c1cd6e05cfa72
ShuTing Hu <shutinghu2000@gmail.com>
2026-03-20 00:20:11 -0700

    native system scrollbar behaviour

--- src/electron/renderer/components/left-sidebar.tsx
+++ src/electron/renderer/components/left-sidebar.tsx
@@ -334,7 +334,7 @@ export function LeftSidebar({
           <div className="flex-1 min-h-0 flex flex-col">
             <section className="min-h-0 flex-1 flex flex-col">
               <SectionLabel className="mb-2">Live Summary</SectionLabel>
-              <div className="min-h-0 flex-1 overflow-y-auto pr-1">
+              <div className="min-h-0 flex-1 overflow-y-auto">
                 {liveSummaryPoints.length > 0 ? (
                   <ul className="space-y-1.5">
                     {liveSummaryPoints.map((point, i) => (
